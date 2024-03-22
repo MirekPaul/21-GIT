66059c4ee84112d326ee6596adc17c2d5b9f34dc
MirekPaul <mirekpaul@seznam.cz>
2024-03-22 11:45:25 +0100

    vstupni funkce

---
+++ Paul-GIT.js
@@ -0,0 +1,11 @@
+function vstup(){
+    let jmenoUzivatele = prompt("Zadejte sve jmeno:");
+    console.log(jmenoUzivatele);
+    if(jmenoUzivatele==""){
+        console.log("Prázdné");
+    }
+}
+
+//START:
+
+vstup();
