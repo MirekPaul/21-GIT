a7453e40c292791564a01a3f93b97318794fc24a
MirekPaul <mirekpaul@seznam.cz>
2024-03-23 08:58:36 +0100

    funkce upravJmeno a zpetny cyklus

--- Paul-GIT.js
+++ Paul-GIT.js
@@ -1,11 +1,30 @@
 function vstup(){
-    let jmenoUzivatele = prompt("Zadejte sve jmeno:");
-    console.log(jmenoUzivatele);
-    if(jmenoUzivatele==""){
-        console.log("Prázdné");
+    //funkce se uzivatele zepta na jmeno a nasledne overi, ze neco zadal
+    let jmeno 
+    
+    while (true){ //cyklus neskonci, dokud uzivatel neco nezada
+        jmeno = prompt("Zadejte sve jmeno:");
+        
+        jmeno=jmeno.trim();
+        console.log(jmeno);
+
+        if(jmeno != ""){
+            break;
+        }
+    }
+
+    return jmeno;
+}
+
+function upravJmeno(upraveneJmeno){
+    //funkce odstrani zbytecne mezery a prevede vsechan prvni pismena na velka
+
+    for (let i = upraveneJmeno.length-1; i >=0 ; i--){
+        console.log(upraveneJmeno[i]);
     }
 }
 
 //START:
 
-vstup();
+let jmenoUzivatele = vstup();
+upravJmeno(jmenoUzivatele);
